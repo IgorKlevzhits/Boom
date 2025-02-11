ed3330a1c33acf955369f42ad125a8989ff3d8b5
Artem Kriukov <art.kriukov@gmail.com>
2025-02-11 20:52:41 +0300

    refactor ViewController to MVC

--- Boom/Controllers/GameViewController.swift
+++ Boom/Controllers/GameViewController.swift
@@ -10,100 +10,26 @@ import AVFoundation
 import ImageIO
 
 class GameViewController: UIViewController {
-    // MARK: - UI
-    private lazy var backgroundImageView: UIImageView = {
-        let element = UIImageView(image: UIImage(named: "WhiteBackground"))
-        element.contentMode = .scaleAspectFill
-        element.translatesAutoresizingMaskIntoConstraints = false
-        return element
-    }()
-    
-    private lazy var titleLabel: UILabel = {
-        let element = UILabel()
-        element.text = "Нажмите “Запустить” чтобы начать игру"
-        element.numberOfLines = 0
-        element.textColor = UIColor(named: "TextColor")
-        element.textAlignment = .center
-        element.font = UIFont(name: Fonts.SFMedium, size: 28)
-        element.translatesAutoresizingMaskIntoConstraints = false
-        return element
-    }()
-    
-    private lazy var bombImageView: UIImageView = {
-        let element = UIImageView()
-        element.contentMode = .scaleAspectFit
-        element.translatesAutoresizingMaskIntoConstraints = false
-        return element
-    }()
-    
-    private lazy var startGameButton: UIButton = {
-        let element = UIButton(type: .system)
-        element.setTitle("Запустить", for: .normal)
-        element.titleLabel?.font = UIFont(name: Fonts.SFHeavy, size: 20)
-        element.tintColor = UIColor(named: "TextColor")
-        element.backgroundColor = UIColor(named: "GameButtons")
-        element.layer.cornerRadius = 10
-        element.translatesAutoresizingMaskIntoConstraints = false
-        return element
-    }()
-    
-    
-    private lazy var pauseButtonNavBar: UIBarButtonItem = {
-        let button = UIButton(type: .custom)
-        button.setImage(UIImage(systemName: "pause.circle",
-                                withConfiguration: UIImage.SymbolConfiguration(pointSize: 35)),
-                        for: .normal)
-        button.tintColor = UIColor(named: "TextColor")
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.widthAnchor.constraint(equalToConstant: 35).isActive = true
-        button.heightAnchor.constraint(equalToConstant: 35).isActive = true
-        button.addTarget(self, action: #selector(pauseButtonTapped), for: .touchUpInside)
-        let element = UIBarButtonItem(customView: button)
-        return element
-    }()
-    
-    private lazy var titleNavBar: UILabel = {
-        let element = UILabel()
-        element.text = "Игра"
-        element.font = UIFont(name: Fonts.SFBlack, size: 30)
-        element.textColor = .black
-        return element
-    }()
-    
-    private lazy var backButtonNavBar: UIBarButtonItem = {
-        let button = UIButton(type: .custom)
-        button.setImage(UIImage(systemName: "chevron.backward",
-                                withConfiguration: UIImage.SymbolConfiguration(pointSize: 30)),
-                        for: .normal)
-        button.tintColor = UIColor(named: "TextColor")
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.widthAnchor.constraint(equalToConstant: 30).isActive = true
-        button.heightAnchor.constraint(equalToConstant: 30).isActive = true
-        button.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
-        let element = UIBarButtonItem(customView: button)
-        return element
-    }()
     
     // MARK: - Private Properties
+    private var pauseButtonItem: UIBarButtonItem!
     
-    private var isPause = false
-    private var timer = Timer()
-    private var backgroundPlayer: AVAudioPlayer?
-    private var bombTimerPlayer: AVAudioPlayer?
-    private var isBombSoundPlayed = false
-    private var totalTime = 15
-    private var secondPassed = 0
+    private let gameView = GameView()
+    private let gameModel = GameModel()
     
     // MARK: - Life Circle
     override func viewDidLoad() {
         super.viewDidLoad()
-        setViews()
-        setupConstraints()
+        self.view = gameView
+        
+        setupNavBar()
+        
+        gameView.startGameButton.addTarget(self, action: #selector(startGameButtonTapped), for: .touchUpInside)
         
         try? AVAudioSession.sharedInstance().setCategory(.ambient, mode: .default, options: [.mixWithOthers])
         try? AVAudioSession.sharedInstance().setActive(true)
         
-        playBackgroundMusic()
+        gameModel.playBackgroundMusic()
         loadGIF(named: "AnimationBomb", duration: 15.0)
     }
     
@@ -111,81 +37,31 @@ class GameViewController: UIViewController {
     
     @objc private func backButtonTapped() {
         navigationController?.popViewController(animated: true)
-        timer.invalidate()
+        gameModel.timer.invalidate()
         dismiss(animated: true)
     }
     
     @objc private func pauseButtonTapped() {
-        isPause.toggle()
+        gameModel.pauseOrResumeTimer()
         
-        if let button = pauseButtonNavBar.customView as? UIButton {
-            let imageName = isPause ? "play.circle" : "pause.circle"
-            let image = UIImage(systemName: imageName, withConfiguration: UIImage.SymbolConfiguration(pointSize: 35))
-            button.setImage(image, for: .normal)
-        }
+        let imageName = self.gameModel.isPaused ? "play.circle" : "pause.circle"
+//        if let button = pauseButtonItem.customView as? UIButton {
+//            let image = UIImage(systemName: imageName, withConfiguration: UIImage.SymbolConfiguration(pointSize: 35))
+//            button.setImage(image, for: .normal)
+//        }
     }
     
-    private func pauseOrResumeTimer() {
-        if isPause {
-            startTimer()
-        } else {
-            timer.invalidate()
-        }
-        
-        isPause.toggle()
-    }
-    
-    private func startTimer() {
-        timer.invalidate()
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
-    }
+
     
     @objc private func startGameButtonTapped(_ sender: UIButton) {
-        timer.invalidate()
+        gameModel.stopTimer()
         
-        startGameButton.isHidden = true
+        gameView.startGameButton.isHidden = true
         
-        startTimer()
+        gameModel.startTimer()
     }
     
-    @objc private func updateTimer() {
-        if isPause { return }
-        
-        if totalTime == 4 {
-            bombSoundPlayed()
-            pauseButtonNavBar.isEnabled = false
-        }
-        
-        if totalTime > 0 {
-            totalTime -= 1
-            print(totalTime)
-        } else {
-            timer.invalidate()
-            print("Finish")
-        }
-    }
     
-    private func createPlayer(soundName: String, loop: Bool) -> AVAudioPlayer? {
-        guard let url = Bundle.main.url(forResource: soundName, withExtension: "mp3") else { return nil }
-        
-        do {
-            let player = try AVAudioPlayer(contentsOf: url)
-            player.numberOfLoops = loop ? -1 : 0
-            return player
-        } catch {
-            return nil
-        }
-    }
-    
-    private func bombSoundPlayed() {
-        bombTimerPlayer = createPlayer(soundName: Music.bombTimer, loop: false)
-        bombTimerPlayer?.play()
-    }
-    
-    private func playBackgroundMusic() {
-        backgroundPlayer = createPlayer(soundName: Music.backroundMusic, loop: true)
-        backgroundPlayer?.play()
-    }
     
     private func loadGIF(named name: String, duration: TimeInterval) {
         guard let gifURL = Bundle.main.url(forResource: name, withExtension: "gif"),
@@ -202,51 +78,46 @@ class GameViewController: UIViewController {
             }
         }
         
-        bombImageView.image = UIImage.animatedImage(with: images, duration: duration)
+        gameView.bombImageView.image = UIImage.animatedImage(with: images, duration: duration)
     }
 }
-private extension GameViewController {
+
+extension GameViewController {
     
-    // MARK: - Set Views
-    
-    func setViews() {
-        view.addSubview(backgroundImageView)
-        view.addSubview(titleLabel)
-        view.addSubview(bombImageView)
-        view.addSubview(startGameButton)
-        
-        navigationItem.rightBarButtonItem = pauseButtonNavBar
-        navigationItem.titleView = titleNavBar
-        navigationItem.leftBarButtonItem = backButtonNavBar
-        
-        startGameButton.addTarget(self, action: #selector(startGameButtonTapped), for: .touchUpInside)
+    private func titleNavBar() -> UILabel {
+        let element = UILabel()
+        element.text = "Игра"
+        element.font = UIFont(name: Fonts.SFBlack, size: 30)
+        element.textColor = .black
+        return element
     }
     
-    // MARK: - Setup Constraints
+    private func pauseButtonNavBar() -> UIBarButtonItem {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(systemName: "pause.circle", withConfiguration: UIImage.SymbolConfiguration(pointSize: 35)), for: .normal)
+        button.tintColor = UIColor(named: "TextColor")
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.widthAnchor.constraint(equalToConstant: 35).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 35).isActive = true
+        button.addTarget(self, action: #selector(pauseButtonTapped), for: .touchUpInside)
+        return UIBarButtonItem(customView: button)
+    }
     
-    func setupConstraints() {
-        NSLayoutConstraint.activate([
-            
-            backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor),
-            backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            
-            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 23),
-            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 48),
-            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -48),
-            
-            bombImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            bombImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            bombImageView.widthAnchor.constraint(equalToConstant: 312),
-            bombImageView.heightAnchor.constraint(equalToConstant: 352),
-            
-            startGameButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -28),
-            startGameButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 23),
-            startGameButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -23),
-            startGameButton.heightAnchor.constraint(equalToConstant: 55)
-        ])
+    private func backButtonNavBar() -> UIBarButtonItem {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(systemName: "chevron.backward", withConfiguration: UIImage.SymbolConfiguration(pointSize: 30)), for: .normal)
+        button.tintColor = UIColor(named: "TextColor")
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        button.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        return UIBarButtonItem(customView: button)
+    }
+    
+    private func setupNavBar() {
+        self.navigationItem.titleView = titleNavBar()
+        self.navigationItem.leftBarButtonItem = backButtonNavBar()
+        self.navigationItem.rightBarButtonItem = pauseButtonNavBar()
     }
 
 }
---
+++ Boom/Views/GameView.swift
@@ -0,0 +1,98 @@
+//
+//  GameView.swift
+//  Boom
+//
+//  Created by Artem Kriukov on 11.02.2025.
+//
+
+import UIKit
+
+class GameView: UIView {
+    // MARK: - UI
+    private lazy var backgroundImageView: UIImageView = {
+        let element = UIImageView(image: UIImage(named: "WhiteBackground"))
+        element.contentMode = .scaleAspectFill
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let element = UILabel()
+        element.text = "Нажмите “Запустить” чтобы начать игру"
+        element.numberOfLines = 0
+        element.textColor = UIColor(named: "TextColor")
+        element.textAlignment = .center
+        element.font = UIFont(name: Fonts.SFMedium, size: 28)
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private(set) lazy var bombImageView: UIImageView = {
+        let element = UIImageView()
+        element.contentMode = .scaleAspectFit
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private(set) lazy var startGameButton: UIButton = {
+        let element = UIButton(type: .system)
+        element.setTitle("Запустить", for: .normal)
+        element.titleLabel?.font = UIFont(name: Fonts.SFHeavy, size: 20)
+        element.tintColor = UIColor(named: "TextColor")
+        element.backgroundColor = UIColor(named: "GameButtons")
+        element.layer.cornerRadius = 10
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        setViews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension GameView {
+    // MARK: - Set Views
+    
+    func setViews() {
+        addSubview(backgroundImageView)
+        addSubview(titleLabel)
+        addSubview(bombImageView)
+        addSubview(startGameButton)
+        
+
+       
+    }
+    
+    // MARK: - Setup Constraints
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            
+            backgroundImageView.topAnchor.constraint(equalTo: topAnchor),
+            backgroundImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            backgroundImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            backgroundImageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            titleLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 23),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 48),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -48),
+            
+            bombImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            bombImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            bombImageView.widthAnchor.constraint(equalToConstant: 312),
+            bombImageView.heightAnchor.constraint(equalToConstant: 352),
+            
+            startGameButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -28),
+            startGameButton.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 23),
+            startGameButton.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -23),
+            startGameButton.heightAnchor.constraint(equalToConstant: 55)
+        ])
+    }
+}
