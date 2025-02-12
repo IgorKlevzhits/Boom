5f8bf2e6f4fba59a488f921c347198f2a20c9059
Igor <klevzhits94@icloud.com>
2025-02-13 01:16:10 +0300

    Fix constraints in CategoryView and add selected logic

--- Boom/Controllers/Category/CategoryController.swift
+++ Boom/Controllers/Category/CategoryController.swift
@@ -8,7 +8,7 @@ import UIKit
 
 internal class CategoryController {
     var listCategory = [
-        CategoryItem(isSelected: false, iconName: "smile", title: "О разном"),
+        CategoryItem(isSelected: true, iconName: "smile", title: "О разном"),
         CategoryItem(isSelected: false, iconName: "hobby", title: "Спорт и Хобби"),
         CategoryItem(isSelected: false, iconName: "life", title: "Про жизнь"),
         CategoryItem(isSelected: false, iconName: "influencer", title: "Знаменитости"),
