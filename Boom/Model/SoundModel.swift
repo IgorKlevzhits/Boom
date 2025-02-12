7f8e6c9287a11efa1cf3559d28d4dd0681da140b
Бойтик <e.boytik@yandex.ru>
2025-02-12 20:12:49 +0200

    Создал модели для пикера

--- Boom/Model/SoundModel.swift
+++ Boom/Model/SoundModel.swift
@@ -6,42 +6,57 @@
 //
 
 import Foundation
-
-
-enum BackgroundSoundModel: String {
-    case backgroundOne
-    case backgroundTwo
-    case backgroundThree
-    var soundName: String {
-        switch self {
-        case .backgroundOne : return "backgroundMusicOne"
-        case .backgroundTwo: return "backgroundMusicTwo"
-        case .backgroundThree: return "backgroundMusicThree"
-        }
+class BackgroundSoundModel{
+    let titel: String
+    let formatAudio: String
+    init(titel: String, formatAudio: String) {
+        self.titel = titel
+        self.formatAudio = formatAudio
     }
-}
-enum SoundBombTimerModel: String {
-    case bombTimerOne
-    case bombTimerTwo
-    case bombTimerThree
-    var soundName: String {
-        switch self {
-        case .bombTimerOne : return "bombTimerOne"
-        case .bombTimerTwo: return "bombTimerTwo"
-        case .bombTimerThree: return "bombTimerThree"
-        }
-    }
+    static var mocdata: [BackgroundSoundModel] = [
+        .init(titel: "backgroundMusicOne", formatAudio: "mp3"),
+        .init(titel: "backgroundMusicTwo", formatAudio: "mp3"),
+        .init(titel: "backgroundMusicThree", formatAudio: "mp3")]
+    
 }
 
-enum SoundsBoomModel: String {
-    case boomOne
-    case boomTwo
-    case boomThree
-    var soundName: String {
-        switch self {
-        case .boomOne : return "boomOne"
-        case .boomTwo: return "boomTwo"
-        case .boomThree: return "boomThree"
-        }
+class SoundBombTimerMode{
+    let titel: String
+    let formatAudio: String
+    init(titel: String, formatAudio: String) {
+        self.titel = titel
+        self.formatAudio = formatAudio
     }
+    static var mocdata: [SoundBombTimerMode] = [
+        .init(titel: "bombTimerOne", formatAudio: "mp3"),
+        .init(titel: "bombTimerTwo", formatAudio: "mp3"),
+        .init(titel: "bombTimerThree", formatAudio: "mp3")]
+    
 }
+class SoundsBoomModel{
+    let titel: String
+    let formatAudio: String
+    init(titel: String, formatAudio: String) {
+        self.titel = titel
+        self.formatAudio = formatAudio
+    }
+    static var mocdata: [SoundsBoomModel] = [
+        .init(titel: "boomOne", formatAudio: "mp3"),
+        .init(titel: "boomTwo", formatAudio: "mp3"),
+        .init(titel: "boomThree", formatAudio: "mp3")]
+    
+}
+
+
+//enum SoundsBoomModel: String {
+//    case boomOne
+//    case boomTwo
+//    case boomThree
+//    var soundName: String {
+//        switch self {
+//        case .boomOne : return "boomOne"
+//        case .boomTwo: return "boomTwo"
+//        case .boomThree: return "boomThree"
+//        }
+//    }
+//}
